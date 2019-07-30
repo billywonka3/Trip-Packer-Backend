3db14e7b753e5999b7a70a3d3e158346eb3efdb0
billywonka3 <billywonka3@gmail.com>
2019-07-30 17:28:33 -0400

    init

---
+++ app.js
@@ -0,0 +1,88 @@
+require('dotenv').config();
+
+const bodyParser   = require('body-parser');
+const cookieParser = require('cookie-parser');
+const express      = require('express');
+const favicon      = require('serve-favicon');
+const hbs          = require('hbs');
+const mongoose     = require('mongoose');
+const logger       = require('morgan');
+const path         = require('path');
+const cors         = require('cors');
+const passport     = require('passport');
+const session      = require('express-session');
+
+require('./config/passport-config');
+
+
+mongoose.Promise = Promise;
+mongoose
+  // .connect(process.env.MONGODB_URI, {useNewUrlParser: true})
+  .connect('mongodb://localhost/week8---trip-packer/backend-express/', {useMongoClient: true})
+  .then(() => {
+    console.log(`Connected to Mongo! `)
+  }).catch(err => {
+    console.error('Error connecting to mongo', err)
+  });
+
+const app_name = require('./package.json').name;
+const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
+
+const app = express();
+
+
+// Middleware Setup
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+
+// Express View engine setup
+app.use(require('node-sass-middleware')({
+  src:  path.join(__dirname, 'public'),
+  dest: path.join(__dirname, 'public'),
+  sourceMap: true
+}));
+      
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
+
+
+// default value for title local
+app.locals.title = 'Trip-Packer : Take the First Step on a new Journey';
+
+app.use(session({
+  secret:"some secret goes here",
+  resave: true,
+  saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(cors({
+  credentials: true,
+  origin: 
+  [
+    'http://localhost:3000', 
+    'https://trip-packer.herokuapp.com'
+  ]
+}));
+
+
+// ================ ROUTES ==================
+const index = require('./routes/index');
+app.use('/', index);
+
+const userRoutes = require('./routes/userRoutes');
+app.use('/api/auth', userRoutes);
+
+// const projectRoutes = require('./routes/itemRoutes');
+// app.use('/___', projectRoutes);
+// ==========================================
+
+module.exports = app;
+
